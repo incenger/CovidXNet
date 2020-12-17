cddb1df56b9147fabdd576374a79e35bc5cd32b5
Eddie <dangtam.letan@gmail.com>
2020-12-17 11:00:22 +0000

    feat(model): Add baseline resnet34 pretrained

---
+++ models/baseline.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+
+class ResnetCovidX(nn.Module):
+    """
+    Covid X-ray classification using Resnet-34 backbone
+
+    Args:
+        num_class: number of output classes
+    """
+    def __init__(self, num_class=3):
+        super().__init__()
+        self.num_class = num_class
+        self.model = models.resnet34(pretrained=True)
+        # Replace first conv layer for training grayscale images
+        self.model.conv1 = nn.Conv2d(in_channels=1,
+                                     out_channels=64,
+                                     kernel_size=7,
+                                     stride=2,
+                                     padding=3,
+                                     bias=False)
+        # Replace fully connected layers with an embedding layer
+        self.model.fc = nn.Sequential(
+            nn.Linear(in_features=512, out_features=num_class))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: input image, shape (B, C, H, W)
+
+        Returns:
+            torch.Tensor: output logits tensor, shape (B, num_class)
+        """
+        out: torch.Tensor = self.model(x)
+        return out
