4202d081159be8b529ec18e6472b660f61a27b2d
Eddie <dangtam.letan@gmail.com>
2020-12-25 04:37:46 +0000

    feat(model): Add ConvNetXray

--- covidx/models/baseline.py
+++ covidx/models/baseline.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torchvision import models
 from efficientnet_pytorch import EfficientNet
 
+
 class ResnetCovidX(nn.Module):
     """
     Covid X-ray classification using Resnet-34 backbone
@@ -45,14 +46,77 @@ class EfficientNetCovidXray(nn.Module):
     Args:
         num_class: number of output classes
     """
-
     def __init__(self, num_class=3):
         super().__init__()
         self.num_class = num_class
-        self.model = EfficientNet.from_pretrained('efficientnet-b3', in_channels=1)
+        self.model = EfficientNet.from_pretrained('efficientnet-b2',
+                                                  in_channels=1)
         self.fc = nn.Linear(in_features=1000, out_features=self.num_class)
 
     def forward(self, x):
         out = self.model(x)
         out = self.fc(out)
+        return out
+
+
+class ConvNetXray(nn.Module):
+    """
+    Simple ConvNet
+
+    Reference: https://cs231n.github.io/convolutional-networks/
+    """
+    def __init__(self, num_class=3):
+        super().__init__()
+
+        self.num_class = num_class
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3,
+                      padding=1), nn.ReLU(), nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64,
+                      out_channels=64,
+                      kernel_size=3,
+                      padding=1), nn.ReLU(), nn.BatchNorm2d(64),
+            nn.MaxPool2d((2, 2)))
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64,
+                      out_channels=128,
+                      kernel_size=3,
+                      padding=1), nn.ReLU(), nn.BatchNorm2d(128),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(in_channels=128,
+                      out_channels=128,
+                      kernel_size=3,
+                      padding=1), nn.ReLU(), nn.BatchNorm2d(128),
+            nn.MaxPool2d((2, 2)))
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=128,
+                      out_channels=256,
+                      kernel_size=3,
+                      padding=1), nn.ReLU(), nn.BatchNorm2d(256),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(in_channels=256,
+                      out_channels=256,
+                      kernel_size=3,
+                      padding=1), nn.ReLU(), nn.BatchNorm2d(256),
+            nn.MaxPool2d((2, 2)))
+
+        # 7*7*512
+        self.fc1 = nn.Sequential(
+            nn.Linear(in_features=7 * 7 * 256, out_features=1024), nn.ReLU())
+
+        self.fc2 = nn.Linear(in_features=1024, out_features=self.num_class)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+
+        out = out.reshape(out.shape[0], -1)
+        
+
+        out = self.fc1(out)
+        out = self.fc2(out)
+
         return out
