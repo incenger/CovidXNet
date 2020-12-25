de79611a530870f1dc03b93d28293c943a559965
Eddie <dangtam.letan@gmail.com>
2020-12-25 10:18:40 +0000

    feat(train): Update train scripts

--- scripts/train.py
+++ scripts/train.py
@@ -4,12 +4,20 @@ from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision.datasets import ImageFolder
 from argparse import ArgumentParser
 from covidx.models.baseline_lightning import XRayClassification
+from covidx.dataset import create_balance_dl, xray_augmentation
+
+SEED = 1411
+
 
 def main(args):
+    pl.seed_everything(SEED)
+
     transform = transforms.Compose([
-        transforms.Resize((256, 256)),
+        transforms.Resize((260, 260)),
         transforms.Grayscale(num_output_channels=1),
+        xray_augmentation(),
         transforms.ToTensor(),
+        transforms.Normalize((0.5, ), (0.5, )),
     ])
 
     train_set = ImageFolder('../data/covidx_image_folder/train',
@@ -17,13 +25,18 @@ def main(args):
     valid_set = ImageFolder('../data/covidx_image_folder/validation/',
                             transform=transform)
 
+    # train_loader_folder = create_balance_dl(train_set,
+    #                                         batch_size=32,
+    #                                         num_workers=12)
+
     train_loader_folder = DataLoader(train_set,
-                                     batch_size=24,
+                                     batch_size=32,
                                      shuffle=True,
                                      num_workers=12,
                                      pin_memory=True)
+
     valid_loader_folder = DataLoader(valid_set,
-                                     batch_size=24,
+                                     batch_size=32,
                                      shuffle=False,
                                      num_workers=12,
                                      pin_memory=True)
@@ -32,6 +45,7 @@ def main(args):
 
     trainer = pl.Trainer.from_argparse_args(args)
     trainer.fit(resnetx, train_loader_folder, valid_loader_folder)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
