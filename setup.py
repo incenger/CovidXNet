adb6cf121c462258f0fc7962e81140cad89c9014
Eddie <dangtam.letan@gmail.com>
2021-01-07 15:15:44 +0000

    fix(setup): Add exclude to setup.pyh

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='CovidX Net',
-    version='0.0.0',
+    version='1.0.0',
     include_package_data=True,
+    packages=find_packages(exclude=["data", "scripts"])
     install_requires=[],
     python_requires=">=3.6",
 )
